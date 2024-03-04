27bbe2390b5fb8747acaac0b57491cef06e910bd
rahma-saleh <158757462+rahma-saleh@users.noreply.github.com>
2024-03-04 15:14:20 +0300

    Daily turnover

--- gwz_sales.sql
+++ gwz_sales.sql
@@ -1,2 +1,7 @@
-SELECT *
+SELECT 
+date_date,
+SUM (purchase_cost) AS toatl_purchase_cost,
+SUM (turnover) AS daily_turnover
  FROM `probable-hash-414008.course14.gwz_sales` 
+ group by date_date
+ order by daily_turnover DES
