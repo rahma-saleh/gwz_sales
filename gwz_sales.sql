e55d9840fd087ff65f297906d0b39b7be2e53aba
rahma-saleh <158757462+rahma-saleh@users.noreply.github.com>
2024-03-08 23:20:44 +0300

    Update gwz_sales.sql

--- gwz_sales.sql
+++ gwz_sales.sql
@@ -1,6 +1,6 @@
 SELECT 
 date_date,
-ROUND(SUM(purchase_cos), 2) AS toatl_purchase_cost,
+ROUND(SUM(purchase_cos),2) AS toatl_purchase_cost,
 ROUND(SUM(turnover), 2) AS turnover
  FROM `probable-hash-414008.course14.gwz_sales` 
  group by date_date
