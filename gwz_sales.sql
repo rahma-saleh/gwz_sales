b3d3ebd8a8cb31fa4980e6a9898f84b5cb9df306
rahma-saleh <158757462+rahma-saleh@users.noreply.github.com>
2024-03-08 22:42:12 +0300

    added purchase cost

--- gwz_sales.sql
+++ gwz_sales.sql
@@ -1,7 +1,7 @@
 SELECT 
 date_date,
-SUM (purchase_cost) AS toatl_purchase_cost,
-SUM (turnover) AS daily_turnover
+SUM (turnover) AS turnover,
+SUM (purchase_cost) AS purchase_cost
  FROM `probable-hash-414008.course14.gwz_sales` 
  group by date_date
- order by daily_turnover DES
+ order by date_date
