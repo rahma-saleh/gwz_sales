33419ec245f80fdec3acfeee1befc2f30e86873a
rahma-saleh <158757462+rahma-saleh@users.noreply.github.com>
2024-03-08 23:14:09 +0300

    most recent dates first

--- gwz_sales.sql
+++ gwz_sales.sql
@@ -1,7 +1,7 @@
 SELECT 
 date_date,
-ROUND(SUM(purchase_cos),2) AS toatl_purchase_cost,
-ROUND(SUM(turnover),2) AS turnover
+ROUND(SUM(purchase_cos), 2) AS toatl_purchase_cost,
+ROUND(SUM(turnover), 2) AS turnover
  FROM `probable-hash-414008.course14.gwz_sales` 
  group by date_date
  order by daily_turnover DES
